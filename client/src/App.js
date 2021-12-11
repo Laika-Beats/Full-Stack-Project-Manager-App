0697e51222696091115746598189aa2998949582
Laika-Beats <laikabeatsdenver@gmail.com>
2021-12-10 18:06:38 -0700

    add client to git

---
+++ client/src/App.js
@@ -0,0 +1,28 @@
+import React from "react";
+import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
+import Dashboard from "./components/Dashboard";
+import Navbar from "./components/Layout/Navbar";
+import AddProject from "./components/Project/AddProject";
+import { Provider } from "react-redux";
+import store from "./store";
+import "bootstrap/dist/css/bootstrap.min.css";
+import "./App.css";
+
+function App() {
+  return (
+    <Provider store={store}>
+      <Router>
+        <div className="App">
+          <Navbar />
+          <Dashboard />
+          <Routes>
+            <Route exact path="/dashboard" element={<Dashboard />} />
+            <Route exact path="/addProject" element={<AddProject />} />
+          </Routes>
+        </div>
+      </Router>
+    </Provider>
+  );
+}
+
+export default App;
